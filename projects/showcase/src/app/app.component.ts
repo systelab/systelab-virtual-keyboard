99685aa35526b4636a20a6d58c3625ceb408c31d
Joaquim Vila Bou <jvila2@werfen.com>
2024-05-20 09:28:51 +0200

    Disabled CSS animations when environment is configured to do so

--- projects/showcase/src/app/app.component.ts
+++ projects/showcase/src/app/app.component.ts
@@ -3,6 +3,8 @@ import {
   SystelabVirtualKeyboardConfig,
   SystelabVirtualKeyboardLayouts
 } from 'systelab-virtual-keyboard';
+import { environment } from '../environments/environment';
+
 
 @Component({
   selector: 'app-root',
@@ -12,6 +14,13 @@ import {
 export class AppComponent {
   public vkConfig: SystelabVirtualKeyboardConfig = {
     layout: SystelabVirtualKeyboardLayouts.numeric,
+  }
+
+  ngOnInit() {
+    if (!environment.animationsEnabled) {
+      const body = document.getElementsByTagName('body')[0];
+      body.classList.add('wdio-disable-animations');
+    }
   }
 }
 
