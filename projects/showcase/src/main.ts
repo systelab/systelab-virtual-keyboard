37c7db1ac4910e1d84a613a91117016a6c763934
Francisco Javier Aguado Joaquín <github@fjaguado.com>
2026-01-23 08:03:28 +0100

    Update dependencies
    Use platformBrowser instead of platformBrowserDynamic

--- projects/showcase/src/main.ts
+++ projects/showcase/src/main.ts
@@ -1,5 +1,5 @@
 import { provideZoneChangeDetection } from "@angular/core";
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { platformBrowser } from '@angular/platform-browser';
 
-platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], });
+platformBrowser().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], });
